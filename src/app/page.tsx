f6bff5f2e56d926e9635cacf245043b25adb9e3d
idilTugba <idilgolcuk@gmail.com>
2024-08-22 16:58:21 +0300

    edit css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
   const latestBlogData: BlogDataType[] = await getBlogData();
 
   return (
-    <main className="flex mt-8 gap-5">
+    <main className="flex text-left mt-8 gap-5">
       <LeftSide data={latestBlogData} />
       <CenterSide data={latestBlogData} />
       <RightSide data={latestBlogData} />
