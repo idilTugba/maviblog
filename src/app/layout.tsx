fe30bd15eaa5d0915cc53888121eb3920b81614c
idilTugba <idilgolcuk@gmail.com>
2024-08-22 18:58:38 +0300

    edit css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+// import './globals.css';
+import './styles/styles.scss';
 import { ApolloWrapper } from '@/lib/apollo-wrapper';
 import Header from '@/components/header/header';
 import Footer from '@/components/footer/footer';
