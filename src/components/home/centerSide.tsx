e0de16bc7c65ababfdf59d1879e098f7864ae893
idilTugba <idilgolcuk@gmail.com>
2024-10-04 23:11:56 +0300

    categorie list

--- src/components/home/centerSide.tsx
+++ src/components/home/centerSide.tsx
@@ -2,7 +2,7 @@ import { BlogDataType } from '@/context/blogContext';
 import BlogList from '../blog/blogList';
 
 const CenterSide = ({ data }: { data: BlogDataType[] }) => {
-  data.reverse();
+  // data.reverse();
   return (
     <div className="w-3/5 inline-block">
       <h2 className="pb-4 mb-6 text-3xl font-semibold border-solid border-b-2 w-full border-primary-dark dark:border-primary-light">
--- src/components/home/leftSide.tsx
+++ src/components/home/leftSide.tsx
@@ -2,16 +2,33 @@ import { BlogDataType } from '@/context/blogContext';
 import BlogList from '../blog/blogList';
 import style from './styles.module.scss';
 
+type GroupedDataType = {
+  [key: string]: BlogDataType[];
+};
+
 const LeftSide = ({ data }: { data: BlogDataType[] }) => {
+  const groupedData = data.reduce<GroupedDataType>((acc, item) => {
+    const { category } = item;
+
+    if (!acc[category]) {
+      acc[category] = [];
+    }
+    acc[category].push(item);
+    return acc;
+  }, {});
+
   return (
     <div className="w-1/5 inline-block border-r-[1px] pr-2 border-solid border-primary-dark dark:border-primary-light">
       <h2 className="mb-5 ">TÜM YAZILAR</h2>
-      {/* {data.set(item => item.category).map(item => {
+      {}
+      {Object.keys(groupedData).map((item) => {
         return (
-          <li key={item.category}>{item.category}</li>
-        )
-      })} */}
-      <BlogList data={data} className={style.leftSide} />
+          <div key={`category-${item}`} className="mb-2">
+            <p className="font-bold text-sm">{item}</p>
+            <BlogList data={groupedData[item]} className={style.leftSide} />
+          </div>
+        );
+      })}
     </div>
   );
 };
