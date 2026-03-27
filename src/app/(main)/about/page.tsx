cc7b97ab15ddfe116c49e7785f912bf6ce576626
İdil Tuğba Gölcük <idilgolcuk@gmail.com>
2026-03-27 09:33:50 +0300

    hakkımızda yazısı

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -8,185 +8,28 @@ const AboutPage = () => {
       <p>
         <Image
           className="inline-block float-left mr-6"
-          src="/bergman.png"
+          src="/mavinese.jpeg"
           alt="Mavi Neşe Gölcük"
           width={250}
           height={250}
         />
 
         <span>
-          Consectetur suscipit debitis totam magnam assumenda aliquam excepturi
-          voluptatem? Explicabo cupiditate, commodi quia expedita voluptatum est
-          alias quasi. Dignissimos voluptates molestiae et mollitia quas
-          quibusdam itaque adipisci eveniet nam dolorum.
-        </span>
-        <span>
-          Pariatur exercitationem fugiat nostrum adipisci, cupiditate a facilis
-          est, commodi quis ducimus dolorum. Quisquam, voluptate dolor! Magnam
-          delectus quo, repellendus maxime vel consequatur soluta obcaecati
-          exercitationem minima laudantium pariatur aliquid!
-        </span>
-        <span>
-          Animi atque fugiat et repellat accusamus quae tenetur quas, laudantium
-          ad illum quisquam obcaecati culpa delectus sed, magni asperiores
-          explicabo nihil fugit dolorum! Explicabo numquam veniam quasi repellat
-          quam non?
-        </span>
-        <span>
-          Modi iure temporibus nulla explicabo veritatis natus perferendis quae
-          quas hic. Dolorem eveniet asperiores doloremque porro ducimus beatae
-          laboriosam perspiciatis quis nulla dicta voluptatem, praesentium
-          perferendis rem fuga eius incidunt?
-        </span>
-        <span>
-          Neque minus nostrum dolore magnam quae id omnis voluptatem tempore
-          reprehenderit deleniti labore dolorem inventore natus provident
-          expedita alias asperiores, sed facere qui eos. Quidem doloremque
-          numquam illo minus laboriosam.
-        </span>
-        <span>
-          Explicabo, mollitia quisquam. Iusto fuga ratione inventore eaque porro
-          corporis, omnis est magnam, assumenda ipsa beatae. Dicta assumenda
-          nemo tempora iste eligendi odit, ex porro dignissimos unde quam
-          mollitia animi?
-        </span>
-        <span>
-          Nesciunt, molestias enim. Voluptate ratione, dolore eligendi
-          reiciendis quidem vel distinctio saepe neque deserunt sit ea cumque
-          rerum temporibus officiis incidunt sint perferendis inventore
-          pariatur! Rerum ipsam error vel fugiat!
-        </span>
-      </p>
-      <p>
-        <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-          deserunt quaerat eaque architecto debitis incidunt. Illum
-          necessitatibus consequatur repellendus dignissimos quasi ex eos,
-          debitis voluptates voluptate enim animi exercitationem assumenda!
-        </span>
-        <span>
-          Earum sunt nihil velit reprehenderit repudiandae tempora cupiditate
-          repellendus praesentium ea illo laborum, natus quod officia vero
-          perferendis est debitis atque tenetur similique alias sapiente
-          doloribus voluptates quia quos. Velit.
-        </span>
-        <span>
-          Doloremque, fugit? Deserunt aliquid aut laudantium, sit sint debitis
-          quas veritatis doloremque nobis? Atque saepe aliquid perferendis
-          nesciunt minus tenetur. Officia vel, accusantium voluptates aperiam
-          sint aliquam tenetur similique molestiae?
-        </span>
-        <span>
-          Deserunt sapiente quis magnam reiciendis cumque? Laborum distinctio
-          magni corporis consequatur nostrum numquam reiciendis aspernatur
-          accusamus? Dolorum velit quod ipsa hic fugiat odit fuga dolor odio,
-          labore saepe officia ipsum!
-        </span>
-        <span>
-          Quo vero ipsum debitis accusamus distinctio harum labore expedita
-          accusantium? Nostrum est qui dignissimos harum ea quis inventore
-          aspernatur fugiat dolorum neque magnam sed, nihil dolor aperiam
-          deleniti tempore vitae.
-        </span>
-        <span>
-          Eaque eos, nisi dolore ut numquam obcaecati nobis voluptates hic
-          cumque omnis itaque explicabo ipsa doloribus quas animi similique
-          eveniet accusantium, distinctio nesciunt! Magni aperiam impedit nemo
-          quos omnis quis.
-        </span>
-        <span>
-          Error asperiores quia, aperiam dolorum mollitia possimus
-          necessitatibus beatae nisi delectus velit laborum, deserunt atque
-          doloremque pariatur ipsam eveniet unde quod quidem eos ex quos. Modi
-          fugiat officia molestias recusandae.
-        </span>
-        <span>
-          Cumque dolorum quod nulla velit pariatur, enim minus placeat possimus
-          itaque, reprehenderit inventore ratione, numquam fugit blanditiis
-          neque voluptatem quasi sunt tempore suscipit. Dolor error odio iste
-          deserunt recusandae tenetur?
-        </span>
-        <span>
-          Asperiores excepturi assumenda error voluptatem! Fugiat, nisi?
-          Adipisci minima eius iure dignissimos ratione quia. Aliquam ad iste
-          earum nam minus nisi beatae, iure dignissimos rem molestias magnam
-          natus assumenda vel?
-        </span>
-      </p>
-      <p>
-        <span>
-          Pariatur exercitationem fugiat nostrum adipisci, cupiditate a facilis
-          est, commodi quis ducimus dolorum. Quisquam, voluptate dolor! Magnam
-          delectus quo, repellendus maxime vel consequatur soluta obcaecati
-          exercitationem minima laudantium pariatur aliquid!
-        </span>
-        <span>
-          Animi atque fugiat et repellat accusamus quae tenetur quas, laudantium
-          ad illum quisquam obcaecati culpa delectus sed, magni asperiores
-          explicabo nihil fugit dolorum! Explicabo numquam veniam quasi repellat
-          quam non?
-        </span>
-        <span>
-          Modi iure temporibus nulla explicabo veritatis natus perferendis quae
-          quas hic. Dolorem eveniet asperiores doloremque porro ducimus beatae
-          laboriosam perspiciatis quis nulla dicta voluptatem, praesentium
-          perferendis rem fuga eius incidunt?
-        </span>
-        <span>
-          Neque minus nostrum dolore magnam quae id omnis voluptatem tempore
-          reprehenderit deleniti labore dolorem inventore natus provident
-          expedita alias asperiores, sed facere qui eos. Quidem doloremque
-          numquam illo minus laboriosam.
-        </span>
-        <span>
-          Explicabo, mollitia quisquam. Iusto fuga ratione inventore eaque porro
-          corporis, omnis est magnam, assumenda ipsa beatae. Dicta assumenda
-          nemo tempora iste eligendi odit, ex porro dignissimos unde quam
-          mollitia animi?
-        </span>
-        <span>
-          Nesciunt, molestias enim. Voluptate ratione, dolore eligendi
-          reiciendis quidem vel distinctio saepe neque deserunt sit ea cumque
-          rerum temporibus officiis incidunt sint perferendis inventore
-          pariatur! Rerum ipsam error vel fugiat!
-        </span>
-      </p>
-      <p>
-        <span>
-          Pariatur exercitationem fugiat nostrum adipisci, cupiditate a facilis
-          est, commodi quis ducimus dolorum. Quisquam, voluptate dolor! Magnam
-          delectus quo, repellendus maxime vel consequatur soluta obcaecati
-          exercitationem minima laudantium pariatur aliquid!
-        </span>
-        <span>
-          Animi atque fugiat et repellat accusamus quae tenetur quas, laudantium
-          ad illum quisquam obcaecati culpa delectus sed, magni asperiores
-          explicabo nihil fugit dolorum! Explicabo numquam veniam quasi repellat
-          quam non?
-        </span>
-        <span>
-          Modi iure temporibus nulla explicabo veritatis natus perferendis quae
-          quas hic. Dolorem eveniet asperiores doloremque porro ducimus beatae
-          laboriosam perspiciatis quis nulla dicta voluptatem, praesentium
-          perferendis rem fuga eius incidunt?
-        </span>
-        <span>
-          Neque minus nostrum dolore magnam quae id omnis voluptatem tempore
-          reprehenderit deleniti labore dolorem inventore natus provident
-          expedita alias asperiores, sed facere qui eos. Quidem doloremque
-          numquam illo minus laboriosam.
-        </span>
-        <span>
-          Explicabo, mollitia quisquam. Iusto fuga ratione inventore eaque porro
-          corporis, omnis est magnam, assumenda ipsa beatae. Dicta assumenda
-          nemo tempora iste eligendi odit, ex porro dignissimos unde quam
-          mollitia animi?
-        </span>
-        <span>
-          Nesciunt, molestias enim. Voluptate ratione, dolore eligendi
-          reiciendis quidem vel distinctio saepe neque deserunt sit ea cumque
-          rerum temporibus officiis incidunt sint perferendis inventore
-          pariatur! Rerum ipsam error vel fugiat!
+          1975 Diyarbakır doğumlu olan yazar aslen Çermiklidir. Diyarbakır’da
+          Nükhet Coşkun Akyol ilkokulunda, orta ve lise öğrenimini Ziya Gökalp
+          Lisesinde tamamladı. Ege Üniversitesinde Eczacılık Fakültesi ve
+          Anadolu üniversitesinde Felsefe okudu. Sevim Burak ve Turgut Uyar’ın
+          büyük kapılarından geçerken sihirlendiği edebiyatla ilişkisinin
+          “yazgı” olduğuna karar vererek bu yazgıyı okumaya çalıştı. Bu ça
+          lışmanın ilk sonucu olan 1997-2005 yılları arasında yazdığı öyküleri
+          topladığı kitabı Kar Beyrut Kar, Agora Kitaplığı’ndan 2006’da
+          yayımlandı. İkinci sonucu olan Soğuk Ses, ilk kitabında da
+          gözlemlediği “kafesler” hakkında bir romandır ve 2014 yılında Duygu
+          Asena roman ödülünde “özgün dili ve psikolojik derinliği “ sebebiyle
+          övgüye değer roman olarak değerlendirilmiştir. Bilim, Bütüncül Tıp,
+          Budizm ve Sufizm ile ilgilenen, Diyarbakır, İzmir, Londra ve
+          İstanbul’da yaşamış olan yazar halen İstanbulda yaşıyor ve kafesleri
+          ve gökleri gözlemeye devam ediyor.
         </span>
       </p>
     </div>
