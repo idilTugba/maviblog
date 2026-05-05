76baf4e1ed444e8cef5dff2b6fc8a60a111fedd8
İdil Tuğba Gölcük <idilgolcuk@gmail.com>
2026-05-05 05:44:41 +0300

    about

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -18,18 +18,7 @@ const AboutPage = () => {
           1975 Diyarbakır doğumlu olan yazar aslen Çermiklidir. Diyarbakır’da
           Nükhet Coşkun Akyol ilkokulunda, orta ve lise öğrenimini Ziya Gökalp
           Lisesinde tamamladı. Ege Üniversitesinde Eczacılık Fakültesi ve
-          Anadolu üniversitesinde Felsefe okudu. Sevim Burak ve Turgut Uyar’ın
-          büyük kapılarından geçerken sihirlendiği edebiyatla ilişkisinin
-          “yazgı” olduğuna karar vererek bu yazgıyı okumaya çalıştı. Bu ça
-          lışmanın ilk sonucu olan 1997-2005 yılları arasında yazdığı öyküleri
-          topladığı kitabı Kar Beyrut Kar, Agora Kitaplığı’ndan 2006’da
-          yayımlandı. İkinci sonucu olan Soğuk Ses, ilk kitabında da
-          gözlemlediği “kafesler” hakkında bir romandır ve 2014 yılında Duygu
-          Asena roman ödülünde “özgün dili ve psikolojik derinliği “ sebebiyle
-          övgüye değer roman olarak değerlendirilmiştir. Bilim, Bütüncül Tıp,
-          Budizm ve Sufizm ile ilgilenen, Diyarbakır, İzmir, Londra ve
-          İstanbul’da yaşamış olan yazar halen İstanbulda yaşıyor ve kafesleri
-          ve gökleri gözlemeye devam ediyor.
+          Anadolu üniversitesinde Felsefe okudu.
         </span>
       </p>
     </div>
